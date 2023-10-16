b7e3bddf657638fa706926758e2e73e6b481ede7
haroldZS <haroldzs1024@gmail.com>
2023-10-16 18:29:29 -0400

    Map Matrix

--- game.js
+++ game.js
@@ -1,11 +1,42 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame);
+let canvasSize;
+let elementSize;
+
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function startGame() {
-  let canvasSize;
+  game.font = elementSize + "px Verdana";
+  game.textAlign = "";
 
+  const image = new Image();
+  image.src = "./assets/img/Three.png";
+
+  const map = maps[0];
+  const mapMatrix = map
+    .trim()
+    .split("\n")
+    .map((row) => row.trim().split(""));
+  console.log(mapMatrix);
+
+  image.onload = function () {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        game.drawImage(
+          image,
+          elementSize * i,
+          elementSize * j,
+          elementSize,
+          elementSize
+        );
+      }
+    }
+  };
+}
+
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -15,22 +46,7 @@ function startGame() {
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
 
-  const elementSize = canvasSize / 10;
-  game.font = elementSize + "px Verdana";
-  game.textAlign = "";
+  elementSize = canvasSize / 10;
 
-  const image = new Image();
-  image.src = "./assets/img/Three.png";
-
-  image.onload = function () {
-    for (let i = 0; i < 10; i++) {
-      game.drawImage(
-        image,
-        elementSize * i,
-        0,
-        elementSize,
-        elementSize
-      );
-    }
-  };
+  startGame();
 }
